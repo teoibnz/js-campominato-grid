a9f301acb429810274753db4a93355f7244e6bd7
Matteo Lopez <matteolopezbb@gmail.com>
2022-03-04 19:45:14 +0100

    refactoring

--- main.js
+++ main.js
@@ -1,21 +1,3 @@
-// Consegna
-
-// L'utente indica un livello di difficoltà in base al quale viene generata una griglia di gioco quadrata, in cui ogni cella contiene un numero tra quelli compresi in un range:
-// con difficoltà 1 => tra 1 e 100
-// con difficoltà 2 => tra 1 e 81
-// con difficoltà 3 => tra 1 e 49
-
-// Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro.
-
-// Consigli del giorno: :party_wizard:
-
-// Scriviamo prima cosa vogliamo fare passo passo in italiano, dividiamo il lavoro in micro problemi.
-// Ad esempio:  di cosa ho bisogno per generare i numeri?
-
-// Proviamo sempre prima con dei console.log() per capire se stiamo ricevendo i dati giusti.
-
-// Le validazioni e i controlli possiamo farli anche in un secondo momento.
-
 
 function selectItem (){
     let selectItem = document.getElementById('level-select');
